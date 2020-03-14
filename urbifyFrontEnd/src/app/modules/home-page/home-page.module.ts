e59ffc9618ab5b6ed0d4777da55bd319adce9965
Santhosh Reddy <santhosh.reddy@quadgen.com>
2020-03-15 00:41:36 +0530

    created professional card

--- urbifyFrontEnd/src/app/modules/home-page/home-page.module.ts
+++ urbifyFrontEnd/src/app/modules/home-page/home-page.module.ts
@@ -4,6 +4,7 @@ import { HomePageComponent } from './home-page.component';
 import { Route, RouterModule, Routes } from '@angular/router';
 import { NbLayoutModule } from '@nebular/theme';
 import { SliderModule } from 'src/app/shared/slider/slider.module';
+import { ProfessionalsCardModule } from 'src/app/shared/professionals-card/professionals-card.module';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent}
@@ -15,6 +16,7 @@ const routes: Routes = [
     CommonModule,
     NbLayoutModule,
     SliderModule,
+    ProfessionalsCardModule,
     RouterModule.forChild(routes)
   ],
   exports: [
---
+++ urbifyFrontEnd/src/app/shared/professionals-card/professionals-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-professionals-card',
+  templateUrl: './professionals-card.component.html',
+  styleUrls: ['./professionals-card.component.scss']
+})
+export class ProfessionalsCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
